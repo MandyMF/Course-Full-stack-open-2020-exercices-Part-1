c49a1402942cc82e518f3d5668a435842fbcc72a
MandyMF <mandy@fonoma.com>
2020-09-08 00:22:49 -0400

    Exercise 1.3

--- course_information/src/index.js
+++ course_information/src/index.js
@@ -11,16 +11,16 @@ const Header =(props)=>{
 const Content =(props)=>{
   return(
     <>
-      <Part part={props.part1} exercise={props.exercises1} />
-      <Part part={props.part2} exercise={props.exercises2} />
-      <Part part={props.part3} exercise={props.exercises3} />
+      <Part part={props.part1} />
+      <Part part={props.part2} />
+      <Part part={props.part3} />
     </>
   )
 }
 
 const Part = (props) =>{
   return (
-  <p>{props.part} {props.exercise}</p>
+  <p>{props.part.name} {props.part.exercises}</p>
   )
 }
 
@@ -33,12 +33,18 @@ const Total =(props)=>{
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
@@ -49,12 +55,9 @@ const App = () => {
       part1={part1} 
       part2={part2}
       part3={part3} 
-      exercises1={exercises1} 
-      exercises2={exercises2} 
-      exercises3={exercises3} 
       />
 
-      <Total total={exercises1 + exercises2 + exercises3}/>
+      <Total total={part1.exercises + part2.exercises + part3.exercises}/>
       
     </div>
   )
